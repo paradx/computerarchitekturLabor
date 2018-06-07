ff5be312c7823a528ba86461de7e28d53c2ef1e6
linus andrae <landrae@stud.hs-bremen.de>
2018-06-07 14:13:58 +0200

    changed aufgabe1

--- Aufgabe1.s
+++ Aufgabe1.s
@@ -2,13 +2,12 @@
 ; ----------------------
 
 Prompt:		.asciiz		"Please enter an integer >1 : \n"
-Prompt2:  .asciiz   "Please enter a floating point number : \n"
 
 PrintfFormat:	.asciiz		"Result: %d.\n"
 				.align		2
 PrintParameter:	.word		PrintfFormat
 Int:	.space 		2
-Floats: .space 2
+Int2: .space    2
 
 			.text
 			.global	main
@@ -19,42 +18,43 @@ main:
 			nop ; required for branch delay slots
 			nop ; required for branch delay slots
       
-      sw Int,r1; mov r1 to Int
+      sw Int,r1           ; mov first number to $Int
 			
       addi	r1,r0,Prompt
 			jal		InputUnsigned ; call of InputUnsigned
 			nop ; required for branch delay slots
 			nop ; required for branch delay slots
       
-      sw Int, r1 ; mov r1 to Int
+      sw Int, r1      ; mov second number to $Int
       
-      addi r1, r0, Prompt2 
-      jal InputFloat 
+      addi r1, r0, Prompt
+      jal InputUnsigned
       nop ; required for branch delay slots
       nop ; required for branch delay slots
       
-      sw Floats, r1 ; mov first floatingpoint number to $Floats
+      sw Int2, r1 ; mov third number to $Int2
       
       addi r1, r0, Prompt2 
       jal InputFloat 
       nop
       nop
       
-      sw Floats, r0 ; mov second floatingpoint number to $Floats
+      sw Int2, r0 ; mov forth number to $Int2
       
       ;Calculate the result
       ;multiply both of the integers
       
       lw r1, Int
       lw r2, Int
-      mult r3, r1,r2
-      lf f1, Floats
-      lf f2, Floats
-      mult f3, f1,f2
+      mult r3, r1,r2 
       
+      lw r1, Int2
+      lw r2, Int3 
+      mult r4, r1, r2 
       
+      mult r1, r3,r4
 			; Output of the calculated result
-			sw		PrintfValue, r1 
+			sw		PrintfFormat, r1 
 			addi	r14, r0, PrintParameter
 			trap	5 
 			; End of program
@@ -72,7 +72,7 @@ main:
 
 		; Data for Read-Trap
 ReadBuffer:	.space		80
-ReadPar:	.word		0,ReadBuffer,80
+ReadPar:	  .word		  0,ReadBuffer,80
 
 		; Data for Printf-Trap
 PrintfPar:	.space		4
@@ -82,9 +82,7 @@ SaveR3:		.space		4
 SaveR4:		.space		4
 SaveR5:		.space		4
 
-
 		.text
-
 		.global		InputUnsigned
 InputUnsigned:	
 		; save register contents
@@ -107,73 +105,12 @@ InputUnsigned:
 		addi		r1,r0,0
 		addi		r4,r0,10	;Decimal system
 
+
+		
+
 Loop:		; reads digits to end of line
 		lbu		r3,0(r2)
     nop ; required because of data dependency on R3
-		seqi		r5,r3,10	;LF -> Exit
-		bnez		r5,Finish
-		nop ; required for branch delay slots
-		nop ; required for branch delay slots
-		subi		r3,r3,48	;´0´
-		multu		r1,r1,r4	;Shift decimal
-		add		r1,r1,r3
-		addi		r2,r2,1 	;increment pointer
-		j		Loop
-		nop ; required for branch delay slots
-		nop ; required for branch delay slots
-		
-Finish: 	; restore old register contents
-		lw		r2,SaveR2
-		lw		r3,SaveR3
-		lw		r4,SaveR4
-		lw		r5,SaveR5
-		jr		r31		; Return
-		nop ; required for branch delay slots
-		nop ; required for branch delay slots
-
-    
-		.data
-
-		; Data for Read-Trap
-ReadBuffer:	.space		80
-ReadPar:	.word		0,ReadBuffer,80
-
-		; Data for Printf-Trap
-PrintfPar2:	.space		4
-
-SaveR2:		.space		4
-SaveR3:		.space		4
-SaveR4:		.space		4
-SaveR5:		.space		4
-
-
-		.text
-
-		.global		InputFloat
-InputFloat:	
-		; save register contents
-		sw		SaveR2,r2
-		sw		SaveR3,r3
-		sw		SaveR4,r4
-		sw		SaveR5,r5
-
-		; Prompt
-		sw		PrintfPar,r1
-		addi		r14,r0,PrintfPar2
-		trap		5
-
-		; call Trap-3 to read line
-		addi		r14,r0,ReadPar2 ; read float from r14?? 
-		trap		3
-
-		; determine value
-		addi		r2,r0,ReadBuffer
-		addi		r1,r0,0
-		addi		r4,r0,10	;Decimal system
-
-Loop:		; reads digits to end of line
-		lbu		r3,0(r2)
-                nop ; required because of data dependency on R3
 		seqi		r5,r3,10	;LF -> Exit
 		bnez		r5,Finish
 		nop ; required for branch delay slots
